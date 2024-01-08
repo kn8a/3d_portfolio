153bd16107bfa0b855bdb09a67f1b8c24494d114
kn8a <mydreamhomefl@gmail.com>
2024-01-07 23:08:16 -0500

    added sky

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Canvas } from "@react-three/fiber"
 import { Suspense } from "react"
 import Loader from "../components/Loader"
 import Island from "../models/Island"
+import Sky from "../models/Sky"
 
 const Home = () => {
   const adjustIslandForScreenSize = () => {
@@ -31,11 +32,10 @@ const Home = () => {
       >
         <Suspense fallback={<Loader/>}>
 
-          <directionalLight/>
-          <ambientLight/>
-          <pointLight/>
-          <spotLight/>
-          <hemisphereLight/>
+          <directionalLight position={[1,1,1]} intensity={2}/>
+          <ambientLight intensity={0.2}/>
+          <hemisphereLight skyColor='#b1e1ff' groundColor={'#000000'} intensity={1}/>
+          <Sky/>
           <Island
             position={islandPosition}
             scale={islandScale}
